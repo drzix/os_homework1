ca323e81b39d61d12c3dce888e70c22c03a3a947
drzix <korea.drzix@gmail.com>
2018-05-02 21:18:56 +0900

    sched: round robin 로직 추가
    Signed-off-by: drzix <korea.drzix@gmail.com>

--- sched.c
+++ sched.c
@@ -3,7 +3,7 @@
 
 struct time_info get_fcfs_time(const struct job_head *head)
 {
-        const struct job_info *jobs = head->jobs;
+        struct job_info *jobs = head->jobs;
         const int jcnt = head->job_cnt;
         struct time_info info = {
                 .tard_time = 0,
@@ -12,15 +12,16 @@ struct time_info get_fcfs_time(const struct job_head *head)
         sched_time_t now = jobs->arrived;
 
         for (int i = 0; i < jcnt; i++) {
-                sched_job(&info, now, jobs + i);
-                now += (jobs + i)->amount_time;
+                if ((jobs + i)->arrived > now)
+                    now = (jobs + i)->arrived;
+                now += sched_job(&info, now, jobs + i);
         }
 
         return info;
 }
 
-void push_wait_job(struct rb_root *root, const struct job_info *job,
-                   const int idx)
+void sjf_push_wait_job(struct rb_root *root, const struct job_info *job,
+                       const int idx)
 {
         struct wait_job *new = malloc(sizeof(struct wait_job));
         struct rb_node **node = &(root->rb_node), *parent = NULL;
@@ -29,7 +30,7 @@ void push_wait_job(struct rb_root *root, const struct job_info *job,
         new->idx = idx;
         while (*node) {
                 struct wait_job *this = container_of(*node, struct wait_job,
-                                                     wait_node);
+                                                     sjf_node);
                 parent = *node;
                 if (job->amount_time < this->job->amount_time)
                         node = &((*node)->rb_left);
@@ -41,23 +42,24 @@ void push_wait_job(struct rb_root *root, const struct job_info *job,
                         node = &((*node)->rb_right);
         }
 
-        rb_link_node(&new->wait_node, parent, node);
-        rb_insert_color(&new->wait_node, root);
+        rb_link_node(&new->sjf_node, parent, node);
+        rb_insert_color(&new->sjf_node, root);
 }
 
-void pop_wait_job(struct wait_job *wjob, struct rb_root *root)
+void sjf_pop_wait_job(struct wait_job *wjob, struct rb_root *root)
 {
-        rb_erase(&wjob->wait_node, root);
+        rb_erase(&wjob->sjf_node, root);
         free(wjob);
 }
 
 #define job_arrived(now, job)        ((job)->arrived <= (now))
-#define pull_arrived_jobs(start, job_cnt, now, jobs, wait_tree)               \
+#define sjf_pull_arrived_jobs(trav, job_cnt, now, jobs, wait_tree)            \
         do {                                                                  \
-                for (int i = (start); i < (job_cnt); i++) {                   \
+                for (int i = (trav); i < (job_cnt); i++) {                    \
                         if (job_arrived((now), (jobs) + i)) {                 \
-                                push_wait_job((wait_tree), (jobs) + i, i);    \
-                                (start)++;                                    \
+                                sjf_push_wait_job((wait_tree),                \
+                                                  (jobs) + i, i);             \
+                                (trav)++;                                     \
                         } else {                                              \
                                 break;                                        \
                         }                                                     \
@@ -66,7 +68,7 @@ void pop_wait_job(struct wait_job *wjob, struct rb_root *root)
 
 struct time_info get_sjf_time(const struct job_head *head)
 {
-        const struct job_info *jobs = head->jobs;
+        struct job_info *jobs = head->jobs;
         const int jcnt = head->job_cnt;
         struct time_info info = {
                 .tard_time = 0,
@@ -79,27 +81,100 @@ struct time_info get_sjf_time(const struct job_head *head)
         int trav = 0;
 
         do {
-                pull_arrived_jobs(trav, jcnt, now, jobs, &wait_tree);
+                sjf_pull_arrived_jobs(trav, jcnt, now, jobs, &wait_tree);
                 if (RB_EMPTY_ROOT(&wait_tree)) {
                         now = (jobs + trav)->arrived;
                         continue;
                 }
                 next_wjob = get_shortest_job(&wait_tree);
-                sched_job(&info, now, next_wjob->job);
-                now += next_wjob->job->amount_time;
-                pop_wait_job(next_wjob, &wait_tree);
+                now += sched_job(&info, now, next_wjob->job);
+                sjf_pop_wait_job(next_wjob, &wait_tree);
         } while (!RB_EMPTY_ROOT(&wait_tree) || trav < jcnt);
 
         return info;
 }
 
+#define first_sched(wjob)   ((wjob)->run_time == 0)
+#define job_done(wjob)  ((wjob)->run_time == (wjob)->job->amount_time)
+
+sched_time_t rr_sched_job(struct time_info *info, const int now,
+                          struct wait_job *wjob)
+{
+        sched_time_t rest_time = wjob->job->amount_time - wjob->run_time;
+        sched_time_t perf_time = min(rest_time, NR_RR_QUANTUM);
+
+        if (first_sched(wjob))
+                info->resp_time += now - wjob->job->arrived;
+
+        wjob->run_time += perf_time;
+        return perf_time;
+}
+
+void rr_push_wait_job(struct list_head *rq, struct job_info *job)
+{
+        struct wait_job *new = malloc(sizeof(struct wait_job));
+
+        new->job = job;
+        new->run_time = 0;
+        list_add_tail(&new->rr_list, rq);
+}
+
+void rr_repush_wait_job(struct list_head *rq)
+{
+        list_rotate_left(rq);
+}
+
+void rr_pop_wait_job(struct wait_job *wjob, struct time_info *info,
+                     const int now)
+{
+        list_del(&wjob->rr_list);
+        info->tard_time += now - wjob->job->arrived;
+        free(wjob);
+}
+
 struct time_info get_rr_time(const struct job_head *head)
 {
-        const struct job_info *jobs = head->jobs;
+        struct job_info *jobs = head->jobs;
         const int jcnt = head->job_cnt;
         struct time_info info = {
                 .tard_time = 0,
                 .resp_time = 0
         };
+
+        LIST_HEAD(rr_queue);
+        struct wait_job *next_wjob;
+        sched_time_t now = jobs->arrived;
+        int trav = 0;
+
+        do {
+                if (list_empty(&rr_queue)) {
+                        now = (jobs + trav)->arrived;
+                        for (int i = trav; i < jcnt; i++) {
+                                if (job_arrived(now, jobs + i)) {
+                                        rr_push_wait_job(&rr_queue, jobs + i);
+                                        trav++;
+                                } else
+                                        break;
+                        }
+                        continue;
+                }
+
+                next_wjob = get_rr_next(&rr_queue);
+                now += rr_sched_job(&info, now, next_wjob);
+
+                for (int i = trav; i < jcnt; i++) {
+                        if (job_arrived(now, jobs + i)) {
+                                rr_push_wait_job(&rr_queue, jobs + i);
+                                trav++;
+                        } else
+                                break;
+                }
+
+                if (job_done(next_wjob))
+                        rr_pop_wait_job(next_wjob,&info, now);
+                else
+                        rr_repush_wait_job(&rr_queue);
+        } while (!list_empty(&rr_queue) || trav < jcnt);
+
         return info;
 }
--- sched.h
+++ sched.h
@@ -2,6 +2,7 @@
 #define _SCHED_H
 
 #include "rbtree.h"
+#include "list.h"
 
 typedef int                     sched_time_t;
 
@@ -24,29 +25,49 @@ extern struct time_info get_fcfs_time(const struct job_head *head);
 extern struct time_info get_sjf_time(const struct job_head *head);
 extern struct time_info get_rr_time(const struct job_head *head);
 
-static inline void sched_job(struct time_info *info,
-                             const int now,
-                             const struct job_info *job)
+static inline sched_time_t sched_job(struct time_info *info,
+                                     const int now,
+                                     const struct job_info *job)
 {
         sched_time_t resp_time = now - job->arrived;
 
         info->resp_time += resp_time;
         info->tard_time += resp_time + job->amount_time;
+
+        return job->amount_time;
 }
 
 struct wait_job {
         const struct job_info           *job;
+
+        struct rb_node                  sjf_node;
         int                             idx;
-        struct rb_node                  wait_node;
+
+        struct list_head                rr_list;
+        sched_time_t                    run_time;
 };
 
 static inline struct wait_job *get_shortest_job(struct rb_root *root)
 {
-        return container_of(rb_first(root), struct wait_job, wait_node);
+        return container_of(rb_first(root), struct wait_job, sjf_node);
 }
 
-extern void push_wait_job(struct rb_root *root, const struct job_info *job,
-                          const int idx);
-extern void pop_wait_job(struct wait_job *wjob, struct rb_root *root);
+extern void sjf_push_wait_job(struct rb_root *root, const struct job_info *job,
+                              const int idx);
+extern void sjf_pop_wait_job(struct wait_job *wjob, struct rb_root *root);
+
+#define NR_RR_QUANTUM   4
+
+static inline struct wait_job *get_rr_next(struct list_head *rq)
+{
+        return container_of(rq->next, struct wait_job, rr_list);
+}
+
+extern sched_time_t rr_sched_job(struct time_info *info, const int now,
+                                 struct wait_job *wjob);
+extern void rr_push_wait_job(struct list_head *rq, struct job_info *job);
+extern void rr_repush_wait_job(struct list_head *rq);
+extern void rr_pop_wait_job(struct wait_job *wjob, struct time_info *info,
+                            const int now);
 
 #endif
